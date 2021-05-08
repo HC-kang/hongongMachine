f3410cafb91105ddc0ced249b96b05efab2fd6c9
HC-kang <weston0713@gmail.com>
2021-05-08 23:05:50 +0900

    0508

--- [05-1]Tree.py
+++ [05-1]Tree.py
@@ -45,3 +45,37 @@ from sklearn.tree import plot_tree
 plt.figure(figsize=(10,7))
 plot_tree(dt)
 plt.show()
+
+plt.figure(figsize = (10, 7))
+plot_tree(dt, max_depth = 1, filled=True, feature_names = ['alcohol', 'sugar', 'pH'])
+plt.show()
+
+dt = DecisionTreeClassifier(max_depth=3, random_state=42)
+dt.fit(train_scaled, train_target)
+print(dt.score(train_scaled, train_target))
+print(dt.score(test_scaled, test_target))
+
+plt.figure(figsize = (20, 15))
+plot_tree(dt, filled = True, feature_names = ['alcohol', 'sugar', 'pH'])
+plt.show()
+
+dt = DecisionTreeClassifier(max_depth = 3, random_state = 42)
+dt.fit(train_input, train_target)
+print(dt.score(train_input, train_target))
+print(dt.score(test_input, test_target))
+
+plt.figure(figsize = (20, 15))
+plot_tree(dt, filled=True, feature_names=['alcohol', 'sugar', 'pH'])
+plt.show
+
+print(dt.feature_importances_)
+
+######
+dt = DecisionTreeClassifier(min_impurity_decrease=0.0005, random_state=42)
+dt.fit(train_input, train_target)
+print(dt.score(train_input, train_target))
+print(dt.score(test_input, test_target))
+
+plt.figure(figsize=(20, 15), dpi = 300)
+plot_tree(dt, filled=True, feature_names=['alcohol', 'sugar','pH'])
+plt.show()
